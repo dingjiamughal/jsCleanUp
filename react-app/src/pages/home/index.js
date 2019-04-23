a9e5c95b55808821ba119ac97083dcd3be21e533
dingjiamughal <465560882@qq.com>
2019-04-24 00:24:16 +0800

    login form

--- react-app/src/pages/home/index.js
+++ react-app/src/pages/home/index.js
@@ -1,52 +1,71 @@
 import React, {Component} from 'react';
-import {Form, Icon, Input, Button, Checkbox} from 'antd';
+import {
+    Form,
+    Icon,
+    Input,
+    Button,
+    Checkbox
+} from 'antd';
 export default class Home extends Component {
+    handleSubmit = (user) => {
+        console.log(user);
+    }
+
     render() {
-        const {getFieldDecorator} = this.props.form;
         return (
             <div className='home-page'>
                 <div className='login-form'>
                     <h1>hello world</h1>
-                    <Form onSubmit={this.handleSubmit} className="login-form">
-                        <Form.Item>
-                            {getFieldDecorator('userName', {
-                                rules: [{required: true, message: 'Please input your username!'}]
-                            })(
-                                <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}} />} placeholder="Username" />
-                            )}
-                        </Form.Item>
-                        <Form.Item>
-                            {getFieldDecorator('password', {
-                                rules: [{required: true, message: 'Please input your Password!'}],
-                            })(
-                                <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}} />} type="password" placeholder="Password" />
-                            )}
-                        </Form.Item>
-                        <Form.Item>
-                            {getFieldDecorator('remember', {
-                                valuePropName: 'checked',
-                                initialValue: true
-                            })(
-                                <Checkbox>Remember me</Checkbox>
-                            )}
-                            <a className="login-form-forgot" href="">Forgot password</a>
-                            <Button type="primary" htmlType="submit" className="login-form-button">
-                                Log in
-                            </Button>
-                            Or <a href="">register now!</a>
-                        </Form.Item>
-                    </Form>
+                    <WrapperedUserForm onSubmit={this.handleSubmit}/>
                 </div>
             </div>
-        );
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-            }
-        });
+            );
     }
 }
+
+class UserForm extends Component {
+    render() {
+        const {getFieldDecorator} = this.props.form;
+        return (
+            <Form onSubmit={(e) => {
+                e.preventDefault();
+                this.props.onSubmit(
+                    this.props.form.getFieldsValue()
+                )
+            }}>
+                <Form.Item>
+                    {
+                        getFieldDecorator('username', {
+                            rules: [{required: true, message: '请输入用户名'}]
+                        })(
+                            <Input prefix={<Icon type='user' />} placeholder='Username' />
+                        )
+                    }
+                </Form.Item>
+                <Form.Item>
+                    {getFieldDecorator('password', {
+                        rules: [{required: true, message: '请输入密码'}]
+                    })(
+                        <Input prefix={<Icon type='lock' />} type='password' placeholder='Password' />
+                    )}
+                </Form.Item>
+
+                <Form.Item>
+                    {getFieldDecorator('remember', {
+                        valuePropName: 'checked',
+                        initialValue: true
+                    })(
+                        <Checkbox>Remember me</Checkbox>
+                    )}
+                    <a className='login-form-forgot' href=''>Forgot password</a>
+                    <Button type='primary' htmlType='submit' className='login-form-button'>
+                        Log in
+                    </Button>
+                    Or <a href=''>register now!</a>
+                </Form.Item>
+            </Form>
+        );
+    }
+}
+
+const WrapperedUserForm = Form.create()(UserForm);
